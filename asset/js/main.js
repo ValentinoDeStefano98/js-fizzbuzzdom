9519d7da30682db80e5a8b48d46b4833c55c81ed
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-03-11 18:44:06 +0100

    Effects added

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,13 +1,21 @@
+//seleziono il container
 let container = document.getElementById("containerNumber");
 
 for (let i = 1; i<= 1000; i++){
+    //condizione per verificare se il numero è multiplo di 3 e 5 
     if ((i % 3 == 0) && (i % 5 == 0)){
-        container.innerHTML += `<div class="squareFizzBuzz number p-5">FizzBuzz</div>`;
+        //cambio colore al div ed aggiunta del testo
+        container.innerHTML += `<div class="squareFizzBuzz number cursor zoom">FizzBuzz</div>`;
+    //condizione per verificare se il numero è multiplo di 5    
     } else if(i % 5 == 0){
-        container.innerHTML += `<div class="squareBuzz number p-5">Buzz</div>`;
+        //cambio colore al div ed aggiunta del testo
+        container.innerHTML += `<div class="squareBuzz number cursor zoom">Buzz</div>`;
+    //condizione per verificare se il numero è multiplo di 3
     } else if(i % 3 == 0){
-        container.innerHTML += `<div class="squareFizz number p-5">Fizz</div>`;
+        //cambio colore al div ed aggiunta del testo
+        container.innerHTML += `<div class="squareFizz number cursor zoom">Fizz</div>`;
     } else {
-        container.innerHTML += `<div class="number p-5">${i}</div>`;
+        //colore classico di partenza
+        container.innerHTML += `<div class="number cursor zoom">${i}</div>`;
     }
 }
