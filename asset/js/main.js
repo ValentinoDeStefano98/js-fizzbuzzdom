e8025a85b8ddf4c6bef6cee65eb5309704e789f3
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-03-11 17:57:23 +0100

    colors added

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,13 +1,13 @@
 let container = document.getElementById("containerNumber");
 
 for (let i = 1; i<= 1000; i++){
-    if(i % 3 == 0){
-        container.innerHTML += `<div>Fizz</div>`;
+    if ((i % 3 == 0) && (i % 5 == 0)){
+        container.innerHTML += `<div class="squareFizzBuzz number p-5">FizzBuzz</div>`;
     } else if(i % 5 == 0){
-        container.innerHTML += `<div>Buzz</div>`;
-    } else if (i % 3 == 0 && i % 5 == 0){
-        container.innerHTML += `<div>FizzBuzz</div>`;
+        container.innerHTML += `<div class="squareBuzz number p-5">Buzz</div>`;
+    } else if(i % 3 == 0){
+        container.innerHTML += `<div class="squareFizz number p-5">Fizz</div>`;
     } else {
-        container.innerHTML += `<div>${i}</div>`;
+        container.innerHTML += `<div class="number p-5">${i}</div>`;
     }
 }
